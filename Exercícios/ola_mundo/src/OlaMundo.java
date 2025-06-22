c5af07ab712e3fd043c0c4a7a83da373b8b91e2f
Ray <ray18xyhn@gmail.com>
2025-06-22 17:02:47 -0300

    Ex001

--- Exercícios/ola_mundo/src/OlaMundo.java
+++ Exercícios/ola_mundo/src/OlaMundo.java
@@ -1,7 +1,8 @@
-public class Main {
+public class OlaMundo {
     public static void main(String[] args) {
         
+        int x = 10;
         System.out.println("Olá, Mundo!");
-        
+        System.out.printf("Resultado é: %d", x);
     }
 }
